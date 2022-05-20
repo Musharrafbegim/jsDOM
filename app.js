a904c40377aa6e53e149412bb45415e8465b4442
Musharrafbegim <70473841+Musharrafbegim@users.noreply.github.com>
2022-05-20 19:15:15 +0500

    Final changes to HTML and JS

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 //Immediately invoked function expression
-function resultsBin(){
-    let userInput = Number(document.getElementById("results"));
-    userInput = userInput.toString(2);
-}
-
-function resultsOctal(){
-    let userInput = document.getElementById("results");
-    userInput = userInput.toString(8);
-}
-
-function resultsHex(){
-    let userInput = document.getElementById("results");
-    userInput = userInput.toString(16);
-}
-
+var userInput = Number(prompt("Put a decimal number: "))
 
 
 document.getElementById("resultBtn").onclick = function(){
-    document.getElementById("binary").innerHTML = "Your decimal is " + resultsBin() + " in Binary!"
-    document.getElementById("octal").innerHTML = "Your decimal is " + resultsOctal() + " in Octal!"
-    document.getElementById("hex").innerHTML = "Your decimal is " + resultsHex() + " in Hex!"
-    document.getElementById("refresh").onclick = function(){
-        history.go(0);
-    }
+    document.getElementById("binary").innerHTML = "Your decimal is " + userInput.toString(2) + " in Binary!"
+    document.getElementById("octal").innerHTML = "Your decimal is " + userInput.toString(8) + " in Octal!"
+    document.getElementById("hex").innerHTML = "Your decimal is " + userInput.toString(16) + " in Hex!"
+   
+}
+document.getElementById("refresh").onclick = function(){
+    history.go(0);
 }
